ec553b9dc5452c5c522906f4ca709b8efd1e7b06
Kevin Li <kevinli29033@gmail.com>
2020-08-09 21:51:35 -0700

    2nd iteration of the design

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,7 @@ import { AppLoading } from "expo";
 export default class HomeScreen extends React.Component {
   state = {
     fontsLoaded: false,
+    currentScrollPos: 0,
   };
 
   render() {
@@ -29,32 +30,152 @@ export default class HomeScreen extends React.Component {
 
     return (
       <View>
-        <ScrollView contentContainerStyle={styles.container}>
+        <ScrollView
+          bounces={false}
+          contentContainerStyle={styles.container}
+          scrollEventThrottle={16}
+          onScroll={(event) => {
+            const offset = event.nativeEvent.contentOffset.y;
+            console.log(offset);
+            this.setState({ currentScrollPos: offset });
+          }}
+        >
           <LinearGradient
             colors={["#3f6b6b", "#121212"]}
             style={styles.header}
           />
-          <FlatList style={styles.list} />
+          <FlatList
+            style={styles.list}
+            data={[
+              {
+                key: "0",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "1",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "2",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "3",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "4",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "5",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "6",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "7",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "8",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+              {
+                key: "9",
+                title: "Title",
+                artist: "Artist",
+                album: "Album",
+              },
+            ]}
+            renderItem={({ item }) => (
+              <View style={styles.playlistItem}>
+                <Text style={styles.playlistItemTitle}>{item.title}</Text>
+                <Text
+                  style={styles.playlistItemMeta}
+                >{`${item.artist} • ${item.album}`}</Text>
+              </View>
+            )}
+          />
         </ScrollView>
-        <View style={styles.playlistDetails}>
+        <View
+          style={{
+            ...styles.playlistDetails,
+          }}
+          pointerEvents="none"
+        >
           <Image
-            style={styles.playlistArt}
+            style={this.calculateArtSize()}
             source={{
               uri:
                 "https://github.com/jamiemaison/hosted/blob/master/placeholder.jpg?raw=1",
             }}
           />
-          <Text style={styles.playlistTitle}>Best of 2020</Text>
-          <Text style={styles.playlistSubtitle}>
+          <Text
+            style={{ ...styles.playlistTitle, top: this.calculateTitlePos() }}
+          >
+            Best of 2020
+          </Text>
+          <Text
+            style={{
+              ...styles.playlistSubtitle,
+              top: this.calculateSubtitlePos(),
+            }}
+          >
             {"100,000 LIKES • 2,000,000 VIEWS"}
           </Text>
-          <TouchableOpacity style={styles.playlistButton}>
+          <TouchableOpacity
+            style={{
+              ...styles.playlistButton,
+              top: this.calculateButtonPos(),
+            }}
+          >
             <Text style={styles.playlistButtonText}>SHUFFLE PLAY</Text>
           </TouchableOpacity>
         </View>
       </View>
     );
   }
+
+  calculateTitlePos = () => {
+    return -this.state.currentScrollPos;
+  };
+
+  calculateSubtitlePos = () => {
+    return -this.state.currentScrollPos;
+  };
+
+  calculateButtonPos = () => {
+    return 250 - this.state.currentScrollPos;
+  };
+
+  calculateArtSize = () => {
+    return {
+      width: 148 - this.state.currentScrollPos / 10,
+      height: 148 - this.state.currentScrollPos / 10,
+      opacity: 1 - this.state.currentScrollPos / 350,
+    };
+  };
 }
 
 const fetchFonts = () => {
@@ -71,7 +192,7 @@ const styles = StyleSheet.create({
   },
   header: {
     width: "100%",
-    height: 500,
+    height: 450,
   },
   list: {
     width: "100%",
@@ -80,28 +201,22 @@ const styles = StyleSheet.create({
   },
   playlistDetails: {
     width: "100%",
-    height: 600,
     position: "absolute",
     top: 90,
     display: "flex",
     alignItems: "center",
   },
-  playlistArt: {
-    width: 180,
-    height: 180,
-  },
   playlistTitle: {
     fontFamily: "montserrat-bold",
     color: "#fff",
     fontSize: 30,
-    marginTop: 50,
+    marginTop: 20,
   },
   playlistSubtitle: {
     fontFamily: "montserrat-regular",
     color: "#b9bdbe",
     fontSize: 12,
     marginTop: 15,
-    textTransform: "uppercase",
   },
   playlistButton: {
     backgroundColor: "#2ab759",
@@ -111,12 +226,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 100,
-    marginTop: 40,
+    position: "absolute",
   },
   playlistButtonText: {
     fontFamily: "montserrat-bold",
     fontSize: 12,
     color: "#fff",
     letterSpacing: 2,
+  },
+  playlistItem: {
+    marginLeft: 25,
+    marginBottom: 25,
+  },
+  playlistItemTitle: {
+    fontFamily: "montserrat-bold",
+    fontSize: 18,
+    color: "#fff",
+  },
+  playlistItemMeta: {
+    fontFamily: "montserrat-regular",
+    color: "#b9bdbe",
+    fontSize: 15,
   },
 });
